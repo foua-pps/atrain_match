3ef4fc2907690cd1289768dd7e77a21c340d9e0f
Jakob Malm <jakob.malm@smhi.se>
2010-11-02 15:16:56 +0100

    Use numpy.NaN for no-data values.

--- Ackumulering_stat/orrb_CFC_stat_surfaces.py
+++ Ackumulering_stat/orrb_CFC_stat_surfaces.py
@@ -12,6 +12,7 @@ from orrb_stat_class import OrrbStats
 class CloudFractionSurfacesStats(OrrbStats):
     
     def do_stats(self):
+        from numpy import NaN
         n_clear_clear_csa = 0
         n_clear_cloudy_csa = 0
         n_cloudy_clear_csa = 0
@@ -24,16 +25,14 @@ class CloudFractionSurfacesStats(OrrbStats):
         n_clear_cloudy_cal_MODIS = 0
         n_cloudy_clear_cal_MODIS = 0
         n_cloudy_cloudy_cal_MODIS = 0
-        samples_csa = 0
-        samples_cal = 0
     
         scenes = len(self.results_files)
-        cfc_sum_csa = 0
-        cfc_sum_cal = 0
         
         for datafile in self.results_files:
             current_datafile = open(datafile, "r")
             datalist = current_datafile.readlines()
+            current_datafile.close()
+            
             #print "Datafile: ", datafile
             csa_data = string.split(datalist[4])
             cal_data = string.split(datalist[8])
@@ -60,8 +59,6 @@ class CloudFractionSurfacesStats(OrrbStats):
             n_cloudy_clear_cal_MODIS = n_cloudy_clear_cal_MODIS + int(modis_data[6])
             n_cloudy_cloudy_cal_MODIS = n_cloudy_cloudy_cal_MODIS + int(modis_data[7])
     
-            current_datafile.close()
-        
         samples_csa = n_clear_clear_csa + n_clear_cloudy_csa + n_cloudy_clear_csa +\
                       n_cloudy_cloudy_csa
         samples_cal = n_clear_clear_cal + n_clear_cloudy_cal + n_cloudy_clear_cal +\
@@ -71,9 +68,9 @@ class CloudFractionSurfacesStats(OrrbStats):
             bias_csa_perc = 100.0*float(n_clear_cloudy_csa - n_cloudy_clear_csa)/float(samples_csa)
             mean_CFC_csa = 100.0*(n_cloudy_cloudy_csa+n_cloudy_clear_csa)/samples_csa
         else:
-            bias_csa = -9.0
-            bias_csa_perc = -9.0
-            mean_CFC_csa = -9.0
+            bias_csa = NaN
+            bias_csa_perc = NaN
+            mean_CFC_csa = NaN
         if samples_cal > 0:
             mean_CFC_cal = 100.0*(n_cloudy_cloudy_cal+n_cloudy_clear_cal)/samples_cal
             bias_cal = float(n_clear_cloudy_cal - n_cloudy_clear_cal)/float(samples_cal)
@@ -81,11 +78,11 @@ class CloudFractionSurfacesStats(OrrbStats):
             bias_cal_perc = 100.0*float(n_clear_cloudy_cal - n_cloudy_clear_cal)/float(samples_cal)
             bias_modis_perc = 100.0*float(n_clear_cloudy_cal_MODIS - n_cloudy_clear_cal_MODIS)/float(samples_cal-1)
         else:
-            mean_CFC_cal = -9.0
-            bias_cal = -9.0
-            bias_modis = -9.0
-            bias_cal_perc = -9.0
-            bias_modis_perc = -9.0
+            mean_CFC_cal = NaN
+            bias_cal = NaN
+            bias_modis = NaN
+            bias_cal_perc = NaN
+            bias_modis_perc = NaN
         
         square_sum_csa =  float(n_clear_clear_csa+n_cloudy_cloudy_csa)*bias_csa*bias_csa + \
                          n_cloudy_clear_csa*(-1.0-bias_csa)*(-1.0-bias_csa) + \
@@ -93,21 +90,21 @@ class CloudFractionSurfacesStats(OrrbStats):
         if samples_csa > 0:
             rms_csa = 100.0*math.sqrt(square_sum_csa/(samples_csa-1))
         else:
-            rms_csa = -9.0
+            rms_csa = NaN
         square_sum_cal =  float(n_clear_clear_cal+n_cloudy_cloudy_cal)*bias_cal*bias_cal + \
                          n_cloudy_clear_cal*(-1.0-bias_cal)*(-1.0-bias_cal) + \
                          n_clear_cloudy_cal*(1.0-bias_cal)*(1.0-bias_cal)
         if samples_cal > 0:
             rms_cal = 100.0*math.sqrt(square_sum_cal/(samples_cal-1))
         else:
-            rms_cal = -9.0
+            rms_cal = NaN
         square_sum_modis =  float(n_clear_clear_cal+n_cloudy_cloudy_cal)*bias_modis*bias_modis + \
                            n_cloudy_clear_cal*(-1.0-bias_modis)*(-1.0-bias_modis) + \
                            n_clear_cloudy_cal*(1.0-bias_modis)*(1.0-bias_modis)
         if samples_cal > 0:
             rms_modis = 100.0*math.sqrt(square_sum_modis/(samples_cal-1))
         else:
-            rms_modis = -9.0
+            rms_modis = NaN
         
         self.scenes = scenes
         #self.samples_csa = samples_csa
