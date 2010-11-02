50dfbbe85f9a0ca09909045439ffce774ceb054f
Jakob Malm <jakob.malm@smhi.se>
2010-11-02 15:16:29 +0100

    Removed deprecated functions gather_values_all_months and weifhted_averages from module compile_stats.

--- Ackumulering_stat/compile_stats.py
+++ Ackumulering_stat/compile_stats.py
@@ -28,32 +28,6 @@ def compile_basic_stats(results_files):
     return cfc_results, cth_results, cty_results
 
 
-def gather_values_all_months(cfc_results, cth_results):
-    raise NotImplementedError("This function needs correction and is currently disabled.")
-    all_values = {}
-    for results_type, name in [(cfc_results, 'CFC'), (cth_results, 'CTH')]:
-        for results in results_type.values():
-            for k, v in results.items():
-                if type(v) is float:
-                    try:
-                        all_values['%s.%s' % (name, k)].append(v)
-                    except KeyError:
-                        all_values['%s.%s' % (name, k)] = [v]
-    return all_values, tot_samples
-
-
-def weighted_averages(vals, num_key):
-    """Calculate weighted averages of all values in dict *vals*, based on *num_key*."""
-    raise NotImplementedError("This function needs correction and is currently disabled.")
-    import numpy
-    tot_samples = vals[num_key]
-    grand_tot_samples = sum(tot_samples)
-    averages = {}
-    for k, v_list in vals.items():
-        averages[k] = sum(numpy.multiply(v_list, tot_samples)) / grand_tot_samples
-    
-    return averages
-
 if __name__ == '__main__':
     from glob import glob
     results_files = []
@@ -66,9 +40,5 @@ if __name__ == '__main__':
         results_files.extend(glob("%s/*.dat" % basic_indata_dir))
     print('')
     
-    cfc_results, cth_results, cty_results = compile_stats(results_files)
-    #all_values, tot_samples = gather_values_all_months(cfc_results, cth_results)
-    #averages = weighted_averages(all_values, tot_samples)
-    #for k in sorted(averages.keys()):
-    #    print("%s: %.3f" % (k, averages[k]))
+    cfc_results, cth_results, cty_results = compile_basic_stats(results_files)
 
