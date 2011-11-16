376c552a3222ac384e07b5beba25285e0eef12cc
Jakob Malm <jakob.malm@smhi.se>
2011-11-16 20:24:49 +0100

    Renamed argument 'range' to '_range' in common.elements_within_range() to avoid name conflict with builtin range().

--- common.py
+++ common.py
@@ -9,16 +9,16 @@ class MatchupError(Exception):
     pass
 
 
-def elements_within_range(compare, base, range):
+def elements_within_range(compare, base, _range):
     """Compare arrays *compare* and *base*, elementwise. Returns an array with
-    elements set to True if compare[i] is within (base[i]-range, base[i]+range),
+    elements set to True if compare[i] is within (base[i]-_range, base[i]+_range),
     otherwise false."""
     import numpy
     
     c = numpy.array(compare)
     b = numpy.array(base)
     
-    return numpy.logical_and(c > b - range, c < b + range)
+    return numpy.logical_and(c > b - _range, c < b + _range)
 
 
 def attach_subdir_from_config(finder):
