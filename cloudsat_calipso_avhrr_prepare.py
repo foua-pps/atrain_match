f83708266d0a41eef613dd56abd34c9d8cf444f0
Nina.Hakansson <a001865@c20632.ad.smhi.se>
2016-03-14 15:17:09 +0100

    Bugfix: Also make CalipsoCloudFraction expect non-transposed cloud_top-arrays

--- cloudsat_calipso_avhrr_prepare.py
+++ cloudsat_calipso_avhrr_prepare.py
@@ -2,48 +2,10 @@
 # python cloudsat_calipso_avhrr_prepare.py 
 import numpy as np
 from config import OPTICAL_DETECTION_LIMIT
-def CalipsoCloudFraction(calipsoObj):
-     # This function is obsolete! Now we merge 1 km and 5 km data instead!!
-    # Then we take cloud fraction from mainly 1 km, only from 5 km (=1.0) for thinnest clouds!/KG
-    new_cloud_fraction = np.zeros(calipsoObj.calipso.cloud_fraction.shape, 'd')
-
-    fcf = calipsoObj.calipso.feature_classification_flags
-    new_fcf = np.ones(fcf.shape).astype(fcf.dtype)
-
-    cloud_top = calipsoObj.calipso.cloud_top_profile
-    new_cloud_top = np.ones(cloud_top.shape, 'd')*np.min(cloud_top)
-
-    cloud_base = calipsoObj.calipso.cloud_base_profile
-    new_cloud_base = np.ones(cloud_base.shape, 'd')*np.min(cloud_base)
-
-    optical_depth = calipsoObj.calipso.optical_depth
-    new_optical_depth = np.ones(optical_depth.shape, 'd')*np.min(optical_depth)
-
-    ssccf = calipsoObj.calipso.single_shot_cloud_cleared_fraction
-    new_ssccf = np.ones(ssccf.shape,'d')*np.min(ssccf)
-    for i in range(ssccf.shape[1]):
-        ind = ((ssccf[:,i]>=0) & (ssccf[:,i]<=0.5))
-        
-        new_cloud_top[:,i] = np.where(ind, cloud_top[:,i], new_cloud_top[:,i])
-        new_cloud_base[:,i] = np.where(ind, cloud_base[:,i], new_cloud_base[:,i])
-        new_fcf[:,i] = np.where(ind, fcf[:,i], new_fcf[:,i])
-        new_ssccf[:,i] = np.where(ind, ssccf[:,i], new_ssccf[:,i])
-        if ind.any():
-            new_cloud_fraction[i] = cloud_fraction[i]
-            new_fcf[:,i] = fcf[:,i]
-            new_cloud_top[:,i] = cloud_top[:,i]
-            new_cloud_base[:,i] = cloud_base[:,i]
-            new_optical_depth[:,i] = optical_depth[:,i]
-            new_ssccf[:,i] = ssccf[:,i]
-        
-    return new_cloud_top, new_cloud_base, new_optical_depth, new_cloud_fraction, new_fcf, new_ssccf
-
-
-def NinaTestarMedelCloudBaseAndTop(cloud_top, cloud_base):
-    #Maybe we should use middle of cloud as cloud top. To get better than nonsens comparison for high (optical thin) clouds. Take middel only for clouds the are certainly very thin, deeper than 1km!
-    new_cloud_top = np.where((cloud_top - cloud_base)>1, 0.5*(cloud_top + cloud_base), cloud_top)
-    return new_cloud_top
-
+#Nina 20160314, Removed obsolete function CalipsoCloudFraction.
+#If reintroduced remember that the calipso layer variables where before
+#tranposed! So there might be needed change of indexing 
+# i.e top layer before where in [0,:] and  top layer is now [:,0]
 
 def CloudsatCloudOpticalDepth(cloud_top, cloud_base, optical_depth, cloud_fraction, fcf, min_optical_depth):
     from config import MIN_OPTICAL_DEPTH
@@ -53,34 +15,34 @@ def CloudsatCloudOpticalDepth(cloud_top, cloud_base, optical_depth, cloud_fracti
     new_cloud_fraction = np.zeros(cloud_fraction.shape,'d')
     new_fcf = np.ones(fcf.shape).astype(fcf.dtype)
     
-    for i in range(optical_depth.shape[1]):
+    for pixel_i in range(optical_depth.shape[0]):
 
         depthsum = 0 #Used to sum the optical_depth
-        for j in range(optical_depth.shape[0]):
+        for layer_j in range(optical_depth.shape[1]):
             # Just stops the for loop when there are no more valid value 
-            if optical_depth[j,i] < 0:
+            if optical_depth[pixel_i, layer_j] < 0:
                 break
             else:
-                depthsum = depthsum + optical_depth[j,i]
+                depthsum = depthsum + optical_depth[pixel_i, layer_j]
             
                 # Removes the cloud values for all pixels that have a optical depth (integrated from the top) below MIN_OPTICAL_DEPTH and moves the first valid value to the first column and so on.
                 #if depthsum >= MIN_OPTICAL_DEPTh:
                 if depthsum >= min_optical_depth:
-                    new_cloud_top[0:(optical_depth.shape[0]-j),i] = cloud_top[j:,i]
-                    new_cloud_base[0:(optical_depth.shape[0]-j),i] = cloud_base[j:,i]
-                    # new_cloud_fraction[i] = 1
-                    new_cloud_fraction[i] = cloud_fraction[i] # Let's still trust in what is seen in 1 km data/KG
-                    new_fcf[0:(fcf.shape[0]-j),i] = fcf[j:,i]
+                    new_cloud_top[pixel_i, 0:(optical_depth.shape[1]-layer_j)] = cloud_top[pixel_i, layer_j:]
+                    new_cloud_base[pixel_i, 0:(optical_depth.shape[1]-layer_j)] = cloud_base[pixel_i, layer_j:]
+                    # new_cloud_fraction[pixel_i] = 1
+                    new_cloud_fraction[pixel_i] = cloud_fraction[pixel_i] # Let's still trust in what is seen in 1 km data/KG
+                    new_fcf[pixel_i, 0:(fcf.shape[1]-layer_j)] = fcf[pixel_i, layer_j:]
                     #extrra to get a cloud top that corresponds better to avhrr
-                    for k in range(new_cloud_top.shape[0]):
-                        if new_cloud_top[k, i] < 0:
+                    for k in range(new_cloud_top.shape[1]):
+                        if new_cloud_top[pixel_i, k] < 0:
                             break
-##                        elif new_cloud_top[k, i] - new_cloud_base[k, i] > 2:                  #Let's skip this option
-##                            new_cloud_top[k, i] = new_cloud_base[k, i] + \                    #Not well motivated!/KG
-##                            ((new_cloud_top[k, i] - new_cloud_base[k, i]) * 3/4.)
+##                        elif new_cloud_top[pixel_i,k] - new_cloud_base[pixel_i,k] > 2:                  #Let's skip this option
+##                            new_cloud_top[pixel_i,k] = new_cloud_base[pixel_i,k] + \                    #Not well motivated!/KG
+##                            ((new_cloud_top[pixel_i,k] - new_cloud_base[pixel_i,k]) * 3/4.)
                         else:
-                            new_cloud_top[k, i] = new_cloud_base[k, i] + \
-                            ((new_cloud_top[k, i] - new_cloud_base[k, i]) * 1/2.)
+                            new_cloud_top[pixel_i, k] = new_cloud_base[pixel_i, k] + \
+                            ((new_cloud_top[pixel_i, k] - new_cloud_base[pixel_i, k]) * 1/2.)
                     break
 
     return new_cloud_top, new_cloud_base, new_cloud_fraction, new_fcf
@@ -105,21 +67,17 @@ def check_total_optical_depth_and_warn(caObj):
         print obj.cloud_base_profile[0,badPix]
 
 def CalipsoOpticalDepthHeightFiltering1km(CaObj):
-    #print CaObj.calipso.cloud_top_profile[0,:]
     new_cloud_tops = np.where(
-        CaObj.calipso.cloud_base_profile[0,:] > CaObj.calipso.detection_height_5km,
-        CaObj.calipso.cloud_base_profile[0,:]+0.1,
+        CaObj.calipso.cloud_base_profile[:,0] > CaObj.calipso.detection_height_5km,
+        CaObj.calipso.cloud_base_profile[:,0]+0.1,
         CaObj.calipso.detection_height_5km)
     clouds_to_update = np.logical_and(
-        CaObj.calipso.cloud_top_profile[0,:]>CaObj.calipso.detection_height_5km,
+        CaObj.calipso.cloud_top_profile[:,0]>CaObj.calipso.detection_height_5km,
         np.not_equal(CaObj.calipso.detection_height_5km, -9))
-    CaObj.calipso.cloud_top_profile[0,:] = np.where(
+    CaObj.calipso.cloud_top_profile[:,0] = np.where(
         clouds_to_update,
         new_cloud_tops,
-        CaObj.calipso.cloud_top_profile[0,:])
-    #print CaObj.calipso.cloud_top_profile[1,:]
-    #print CaObj.calipso.cloud_top_profile[0,:]
-    #print CaObj.calipso.detection_height_5km
+        CaObj.calipso.cloud_top_profile[:,0])
     return CaObj
 
 def CalipsoOpticalDepthSetThinToClearFiltering1km(CaObj):
@@ -130,16 +88,16 @@ def CalipsoOpticalDepthSetThinToClearFiltering1km(CaObj):
         CaObj.calipso.number_of_layers_found>0,
         isThin_clouds)
     #print set_to_clear.shape
-    for lay in xrange(CaObj.calipso.cloud_base_profile.shape[0]):
+    for lay in xrange(CaObj.calipso.cloud_base_profile.shape[1]):
         #print lay
-        CaObj.calipso.cloud_top_profile[lay,:] = np.where(
+        CaObj.calipso.cloud_top_profile[:,lay] = np.where(
             set_to_clear,
             -9999,
-            CaObj.calipso.cloud_top_profile[lay,:])
-        CaObj.calipso.cloud_base_profile[lay,:] = np.where(
+            CaObj.calipso.cloud_top_profile[:,lay])
+        CaObj.calipso.cloud_base_profile[:,lay] = np.where(
             set_to_clear,
             -9999,
-            CaObj.calipso.cloud_base_profile[lay,:])
+            CaObj.calipso.cloud_base_profile[:,lay])
     CaObj.calipso.cloud_fraction = np.where(
         set_to_clear,
         0.5,
