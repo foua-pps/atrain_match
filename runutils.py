1885c311e1424166e1da71af6e9145385d2127c4
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2016-12-07 10:29:51 +0100

    Handle bzip2 and gziped files
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- runutils.py
+++ runutils.py
@@ -8,36 +8,83 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def unzip_file(filename):
+    """Unzip the file if file is bzipped = ending with 'bz2'"""
+
+    import tempfile
+    import bz2
+    import gzip
+    if filename.endswith('.bz2'):
+        bz2file = bz2.BZ2File(filename)
+        #tmpfilename = tempfile.mktemp()
+        tmpdir = tempfile.mkdtemp()
+        tmpfilename = os.path.join(tmpdir,
+                                   os.path.basename(filename).strip('.bz2'))
+        try:
+            ofpt = open(tmpfilename, 'wb')
+            ofpt.write(bz2file.read())
+            ofpt.close()
+        except IOError:
+            import traceback
+            traceback.print_exc()
+            logger.info("Failed to read bzipped file %s", str(filename))
+            os.remove(tmpfilename)
+            return None
+
+        return tmpfilename
+
+    elif filename.endswith('.gz'):
+        #tmpfilename = tempfile.mktemp()
+        tmpdir = tempfile.mkdtemp()
+        tmpfilename = os.path.join(tmpdir,
+                                   os.path.basename(filename).strip('.gz'))
+        with gzip.open(filename, 'rb') as f:
+            try:
+                ofpt = open(tmpfilename, 'wb')
+                ofpt.write(f.read())
+                ofpt.close()
+            except IOError:
+                import traceback
+                traceback.print_exc()
+                logger.info("Failed to read bzipped file %s", str(filename))
+                os.remove(tmpfilename)
+                return None
+
+        return tmpfilename
+
+    return None
+
+
 def process_scenes(scenes, fun, options, ignore_errors=True, *args, **kwargs):
     """
     For each string in *scenes*, process corresponding noaa scene, by calling
     function *fun* with arguments satname, orbit, as parsed from *scenes*. Each
     element in *scenes* should look like
     'some/path/noaa19_20100110_1045_04767...'.
-    
+
     If *ignore_errors* is True (default), exceptions will be ignored and
     problematic scenes reported after all scenes have been processed.
-    
+
     Any additional arguments and keyword arguments are passed to *fun*.
-    
+
     """
     errors = []
     for _file in scenes:
         print _file, fun
         filename = os.path.basename(_file)
-        #print filename, fun
+        # print filename, fun
         #satname, _datetime, orbit = parse_scene(filename)
-        logger.info("File : %s"%( filename))
+        logger.info("File : %s" % (filename))
         try:
             fun(_file, options, *args, **kwargs)
         except KeyboardInterrupt:
-                raise    
+            raise
         except Exception, err:
             if ignore_errors:
                 errors.append((filename, err))
             else:
                 raise
-    
+
     if errors:
         logger.info("The following errors were caught")
         for _file, err in errors:
@@ -49,34 +96,35 @@ def parse_scene(filename):
     """
     Parse scene string (e.g. 'noaa19_20100110_1045_04767') and return
     (satname, `datetime.datetime`, orbit).
-    
+
     Examples:
-    
+
     >>> parse_scene('noaa19_20100110_1045_04767')
     ('noaa19', datetime.datetime(2010, 1, 10, 10, 45), 4767)
     >>> parse_scene('noaa19_20100110_1045_04767.okay')
     ('noaa19', datetime.datetime(2010, 1, 10, 10, 45), 4767)
-    
+
     Leading directories are removed:
-    
+
     >>> parse_scene('some/dir/noaa19_20100110_1045_04767')
     ('noaa19', datetime.datetime(2010, 1, 10, 10, 45), 4767)
-    
+
     """
     from datetime import datetime
     import re
     filename = os.path.basename(filename)
     if not filename:
-        raise ValueError("No file %r" % filename)        
+        raise ValueError("No file %r" % filename)
     match = re.match('(\w+)_([0-9]{8})_([0-9]{4})_([0-9]+)', filename)
     if not match:
         raise ValueError("Couldn't parse \"okay\" file %r" % filename)
-    
+
     satname, date_s, time_s, orbit_s = match.groups()
     _datetime = datetime.strptime(date_s + time_s, '%Y%m%d%H%M')
     orbit = int(orbit_s)
-    
+
     return satname, _datetime, orbit
+
 
 def parse_scenesfile_v2014(filename):
     """
@@ -86,14 +134,15 @@ def parse_scenesfile_v2014(filename):
     import re
     filename = os.path.basename(filename)
     if not filename:
-        raise ValueError("No file %r" % filename)        
+        raise ValueError("No file %r" % filename)
     match = re.match(r"S_NWC_.+_([^_]+)_\d+_(\d+)T(\d\d\d\d).+", filename)
     if not match:
         raise ValueError("Couldn't parse pps file %r" % filename)
     satname, date_s, time_s = match.groups()
     _datetime = datetime.strptime(date_s + time_s, '%Y%m%d%H%M')
-    
+
     return satname, _datetime
+
 
 def parse_scenesfile_cci(filename):
     """
@@ -103,14 +152,16 @@ def parse_scenesfile_cci(filename):
     import re
     filename = os.path.basename(filename)
     if not filename:
-        raise ValueError("No file %r" % filename)        
-    match = re.match(r"(\d\d\d\d\d\d\d\d)(\d\d\d\d).+AVHRRGAC-([^-]+)-", filename)
+        raise ValueError("No file %r" % filename)
+    match = re.match(
+        r"(\d\d\d\d\d\d\d\d)(\d\d\d\d).+AVHRRGAC-([^-]+)-", filename)
     if not match:
         raise ValueError("Couldn't parse cci file %r" % filename)
     date_s, time_s, satname = match.groups()
     _datetime = datetime.strptime(date_s + time_s, '%Y%m%d%H%M')
-    
+
     return satname, _datetime
+
 
 def parse_scenesfile_maia(filename):
     """
@@ -120,11 +171,11 @@ def parse_scenesfile_maia(filename):
     import re
     filename = os.path.basename(filename)
     if not filename:
-        raise ValueError("No file %r" % filename)        
+        raise ValueError("No file %r" % filename)
     match = re.match(r"\S\S\SCT_(\S\S\S)_\S\S_(\d+)_S(\d+)_", filename)
     if not match:
         raise ValueError("Couldn't parse maia file %r" % filename)
     satname, date_s, time_s = match.groups()
     _datetime = datetime.strptime(date_s + time_s, '%Y%m%d%H%M%S')
-    
+
     return satname, _datetime
