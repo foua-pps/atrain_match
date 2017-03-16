5c29930889cb76a59ee1a30d18d3a6bc863e3604
Nina.Hakansson <a001865@c20632.ad.smhi.se>
2017-03-16 09:04:32 +0100

    Results written to files always, removed printouts to stdout.
    The flag -w has no longer any effect.
    Program will give message to stop using -w if it is set.

--- compile_stats.py
+++ compile_stats.py
@@ -10,39 +10,32 @@ from config import (CASES, RESOLUTION, _validation_results_dir, AREA,
                     COMPILE_RESULTS_SEPARATELY_FOR_SEMI_AND_OPAQUE,
                     ALSO_USE_5KM_FILES)
 
-def compile_stats(results_files, write=False, outfile_cfc=None):
+import logging
+logging.basicConfig(level=logging.INFO) 
+logger = logging.getLogger(__name__)
+
+def compile_stats(results_files, write=True, outfile_cfc="merged_sat_file_cfc"):
     """Run through all summary statistics."""
     
-    print("=========== Cloud fraction ============")
+    #print("=========== Cloud fraction ============")
     from statistics import orrb_CFC_stat
     cfc_stats = orrb_CFC_stat.CloudFractionStats(results_files)
-    if write:
-        cfc_stats.write(outfile_cfc)
-    for l in cfc_stats.printout():
-        print(l)
+    cfc_stats.write(outfile_cfc)
     
     compiled_cth_file_name = outfile_cfc.replace('_cfc_','_cth_')
     # First all clouds, then split results
     note = "========== Cloud top height ==========="
     from statistics import orrb_CTH_stat
     cth_stats = orrb_CTH_stat.CloudTopStats(results_files, [16,17,18,19], note)
-    if write:
-        cth_stats.write(compiled_cth_file_name)
-    cth_stats.printout()
-    for l in cth_stats.printout():
-        print(l)
+    cth_stats.write(compiled_cth_file_name)
 
     if COMPILE_RESULTS_SEPARATELY_FOR_SINGLE_LAYERS_ETC:
             note = "========== Single Layer     ==========="
             from statistics import orrb_CTH_stat
             cth_stats = orrb_CTH_stat.CloudTopStats(results_files, 
                                                     [21,22,23,24], note)
-            if write:
-                cth_stats.write(compiled_cth_file_name, mode='a')
-            cth_stats.printout()
-            for l in cth_stats.printout():
-                print(l)
-
+            cth_stats.write(compiled_cth_file_name, mode='a')
+            
     if (COMPILE_RESULTS_SEPARATELY_FOR_SINGLE_LAYERS_ETC and
         (ALSO_USE_5KM_FILES or RESOLUTION==5)): 
             note =("=== Single Layer, Not optically thin ==\n"
@@ -50,104 +43,64 @@ def compile_stats(results_files, write=False, outfile_cfc=None):
             from statistics import orrb_CTH_stat
             cth_stats = orrb_CTH_stat.CloudTopStats(results_files, 
                                                     [26,27,28,29], note)
-            if write:
-                cth_stats.write(compiled_cth_file_name, mode='a')
-            cth_stats.printout()
-            for l in cth_stats.printout():
-                print(l)
+            cth_stats.write(compiled_cth_file_name, mode='a')
             note = "===== Top Layer, Not optically very thin ==="
             from statistics import orrb_CTH_stat
             cth_stats = orrb_CTH_stat.CloudTopStats(results_files, 
                                                     [31,32,33,34], note)
-            if write:
-                cth_stats.write(compiled_cth_file_name, mode='a')
-            cth_stats.printout()
-            for l in cth_stats.printout():
-                print(l)
+            cth_stats.write(compiled_cth_file_name, mode='a')
             note = ("==== Top Layer, Optically very thin ===\n"
                     "========== Expect bad results here! ===")
             from statistics import orrb_CTH_stat
             cth_stats = orrb_CTH_stat.CloudTopStats(results_files, 
                                                     [36,37,38,39], note)
-            if write:
-                cth_stats.write(compiled_cth_file_name, mode='a')
-            cth_stats.printout()
-            for l in cth_stats.printout():
-                print(l)
+            cth_stats.write(compiled_cth_file_name, mode='a')
+
     if COMPILE_RESULTS_SEPARATELY_FOR_SEMI_AND_OPAQUE:
         try:
             note = "========== Opaque All     ==========="
             from statistics import orrb_CTH_stat
             cth_stats = orrb_CTH_stat.CloudTopStats(results_files, 
                                                     [41,42,43,44], note)
-            if write:
-                cth_stats.write(compiled_cth_file_name, mode='a')
-            cth_stats.printout()
-            for l in cth_stats.printout():
-                print(l)
+            cth_stats.write(compiled_cth_file_name, mode='a')
             DID_FIND_SEMI_OPAQUE_RESULTS = True     
         except IndexError:
             DID_FIND_SEMI_OPAQUE_RESULTS = False
             print "Can not find opaque/semi ctth results"
-
         if (COMPILE_RESULTS_SEPARATELY_FOR_SEMI_AND_OPAQUE and
             DID_FIND_SEMI_OPAQUE_RESULTS):          
             note = "========== Semi-transparent All ==========="
             from statistics import orrb_CTH_stat
             cth_stats = orrb_CTH_stat.CloudTopStats(results_files, 
                                                     [46,47,48,49], note)
-            if write:
-                cth_stats.write(compiled_cth_file_name, mode='a')
-            cth_stats.printout()
-            for l in cth_stats.printout():
-                print(l)
+            cth_stats.write(compiled_cth_file_name, mode='a')
             note = "========== Opaque Not thin top layer ==========="
             from statistics import orrb_CTH_stat
             cth_stats = orrb_CTH_stat.CloudTopStats(results_files, 
                                                     [51,52,53,54], note)
-            if write:
-                cth_stats.write(compiled_cth_file_name, mode='a')
-            cth_stats.printout()
-            for l in cth_stats.printout():
-                print(l)
+            cth_stats.write(compiled_cth_file_name, mode='a')
             note = "========== Semi-transparent Not thin top layer ==========="
             from statistics import orrb_CTH_stat
             cth_stats = orrb_CTH_stat.CloudTopStats(results_files, 
                                                     [56,57,58,59], note)
-            if write:
-                cth_stats.write(compiled_cth_file_name, mode='a')
-            cth_stats.printout()
-            for l in cth_stats.printout():
-                print(l)
+            cth_stats.write(compiled_cth_file_name, mode='a')
             note = "========== Opaque Thin top layer ==========="
             from statistics import orrb_CTH_stat
             cth_stats = orrb_CTH_stat.CloudTopStats(results_files, 
                                                     [61,62,63,64], note)
-            if write:
-                cth_stats.write(compiled_cth_file_name, mode='a')
-            cth_stats.printout()
-            for l in cth_stats.printout():
-                print(l)
+            cth_stats.write(compiled_cth_file_name, mode='a')
             note = "========== Semi-transparent Thin top layer ==========="
             from statistics import orrb_CTH_stat
             cth_stats = orrb_CTH_stat.CloudTopStats(results_files, 
                                                     [66,67,68,69], note)
-            if write:
-                cth_stats.write(compiled_cth_file_name, mode='a')
-            cth_stats.printout()
-            for l in cth_stats.printout():
-                print(l)
+            cth_stats.write(compiled_cth_file_name, mode='a')
     
     if not CCI_CLOUD_VALIDATION:
-        print("============= Cloud type ==============")
+        #print("============= Cloud type ==============")
         compiled_cty_file_name = outfile_cfc.replace('_cfc_','_cty_')
         from statistics import orrb_CTY_stat
         cty_stats = orrb_CTY_stat.CloudTypeStats(results_files, cfc_stats)
-        if write:
-            cty_stats.write(compiled_cty_file_name)
-        cty_stats.printout()
-        for l in cty_stats.printout():
-            print(l)
+        cty_stats.write(compiled_cty_file_name)
 
 if __name__ == '__main__':
 
@@ -158,21 +111,27 @@ if __name__ == '__main__':
 
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument( '--write', '-w', const=True, nargs='?', required=False,
-                      help="Write results to file")
-    parser.add_argument('--basic', '-b',  const=True, nargs='?', required=False,  
-                      help="Calculate the statistic for mode BASIC")
-    parser.add_argument('--standard', '-t', const=True, nargs='?', required=False,  
-                      help="Calculate the statistic for mode STANDARD")
-    parser.add_argument('--odticfilter', '-o',  const=True, nargs='?', required=False,  
-                      help="Calculate the statistic for mode "
-                      "OPTICAL_DEPTH_THIN_IS_CLEAR")
-    parser.add_argument('--surface_new_way', '-n',  const=True, nargs='?', required=False,
-                      help='calculate the statistic for the different '
-                      'surfaces with basic method')
-    parser.add_argument('--cotfilter', '-c',  const=True, nargs='?', required=False, 
-                      help='calculate the statistic for the optical '
-                      'thickness filters')    
+    parser.add_argument('--basic', '-b',  const=True, nargs='?', 
+                        required=False,  
+                        help="Calculate the statistic for mode BASIC")
+    parser.add_argument('--standard', '-t', const=True, nargs='?', 
+                        required=False,  
+                        help="Calculate the statistic for mode STANDARD")
+    parser.add_argument('--odticfilter', '-o',  const=True, nargs='?', 
+                        required=False,  
+                        help="Calculate the statistic for mode "
+                        "OPTICAL_DEPTH_THIN_IS_CLEAR")
+    parser.add_argument('--surface_new_way', '-n',  const=True, 
+                        nargs='?', required=False,
+                        help='calculate the statistic for the different '
+                        'surfaces')
+    parser.add_argument('--cotfilter', '-c',  const=True, 
+                        nargs='?', required=False, 
+                        help='calculate the statistic for the optical '
+                        'thickness filters')    
+    parser.add_argument( '--write', '-w', const=True, nargs='?', 
+                         required=False, 
+                         help="Depricated: write results to file")
     (options) = parser.parse_args()
 
     #Get filename-templates form atrain_match.cfg
@@ -185,6 +144,8 @@ if __name__ == '__main__':
     #Find all wanted modes (dnt)    
     modes_list =[]
     New_DNT_FLAG = ['', '_DAY', '_NIGHT', '_TWILIGHT']
+    if options.write == True:
+        logger.warning("Results always written to file, -w flag is depricated")
     if options.basic == True:
         modes_list.append('BASIC')
     if options.standard == True:
@@ -197,7 +158,6 @@ if __name__ == '__main__':
         for surface in SURFACES:
             modes_list.append(surface)
         #Add dnt flag to all modes so far
-    print modes_list
     modes_dnt_list = []
     for mode in modes_list:
           for dnt in New_DNT_FLAG:
@@ -210,6 +170,9 @@ if __name__ == '__main__':
                 #modes_list.append("OPTICAL_DEPTH-%0.2_%sf"(dnt,cot))# if like this
                 modes_dnt_list.append("OPTICAL_DEPTH%s-%0.2f"%(dnt,cot))
 
+    if len(modes_dnt_list)==0 :
+        logger.warning("No modes selected!")
+        parser.print_help()
     #For each mode calcualte the statistics            
     for process_mode_dnt in modes_dnt_list:
         print("Gathering statistics from all validation results files in the "
@@ -245,8 +208,9 @@ if __name__ == '__main__':
             stat_type='cfc',
             min_opt_depth="")
         compiled_file_cfc = os.path.join(compiled_dir, compiled_file_cfc)
-        compile_stats(results_files,  write=options.write, outfile_cfc=compiled_file_cfc)
-
+        compile_stats(results_files,  outfile_cfc=compiled_file_cfc)
+    if options.write == True:
+        logger.warning("Results always written to file, -w flag is depricated")
 
 
 
