ec7406fcf017222e3a98120c219fb29e90815198
Nina.Hakansson <a001865@c20632.ad.smhi.se>
2018-10-01 14:40:25 +0200

    merge cloudsat liquid only water path

--- cloudsat.py
+++ cloudsat.py
@@ -247,6 +247,12 @@ def mergeCloudsat(cloudsat, cloudsatlwp):
     cloudsat.RVOD_ice_water_path = np.where(
         cloudsat_lwp_index>=0, 
         cloudsatlwp.RVOD_ice_water_path[index],-9)
+    cloudsat.LO_RVOD_liq_water_path = np.where(
+        cloudsat_lwp_index>=0, 
+        cloudsatlwp.LO_RVOD_liq_water_path[index],-9)
+    cloudsat.IO_RVOD_ice_water_path = np.where(
+        cloudsat_lwp_index>=0, 
+        cloudsatlwp.IO_RVOD_ice_water_path[index],-9)
     return cloudsat
 
 
