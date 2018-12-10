ea04bcfe3a7206cc078f63d89d07a6134c1c4022
Nina.Hakansson <a001865@c20632.ad.smhi.se>
2018-12-10 10:41:15 +0100

    Updates for python3

--- atrain_match/reshaped_files_scr/combine_reshaped_files.py
+++ atrain_match/reshaped_files_scr/combine_reshaped_files.py
@@ -8,39 +8,40 @@ from matchobject_io import (readTruthImagerMatchObj,
 
 instrument = "modis"
 satellite = "eos2"
-truth = "cloudsat"
+truth = "calipso"
 version = "v2018"
 
-BASE_DIR = "/nobackup/smhid14/sm_ninha/atrain_matching/val_v2018_modis_{version}/global_{instrument}_{version}_created20181015_cloudsat_cbase/".format(instrument=instrument, version=version)
-ROOT_DIR = BASE_DIR + "/Reshaped_Files/{satellite}/1km/2010/%s/%s/*{truth}*.h5".format(
+BASE_DIR = "/nobackup/smhid14/sm_ninha/atrain_matching/modis_cloud_base_simon_multilayer_ctth/global_{instrument}_{version}_created20181023/".format(instrument=instrument, version=version)
+ROOT_DIR = BASE_DIR + "/Reshaped_Files/{satellite}/1km/2010/%s/*2010%s%s_*{truth}*.h5".format(
     satellite=satellite, truth=truth)
 OUT_DIR_TEMPLATE = BASE_DIR + "/Reshaped_Files_merged_{truth}/{satellite}/1km/2010/%s/".format(
     truth=truth,satellite=satellite)
 outfile_template = "1km_{satellite}_2010%s%s_%s00_00000_{truth}_{instrument}_match.h5".format(
     satellite=satellite, truth=truth, instrument = instrument)
 
-SETTINGS ={"WRITE_ONLY_THE_MOST_IMPORTANT_STUFF_TO_FILE": True}
+SETTINGS ={"WRITE_ONLY_THE_MOST_IMPORTANT_STUFF_TO_FILE": False}
 
 caObj_night = None
 caObj_day = None
-for year in [2010]
+for year in [2010]:
     for month in ["02","03","04","05","06","07","08","09","10","11","12","01"]:
         for day in ["14", "01"]:
             OUT_DIR = OUT_DIR_TEMPLATE%(month)
             if not os.path.exists(OUT_DIR):
                 os.makedirs(OUT_DIR)
 
-            files = glob(ROOT_DIR%(month,day))
+            files = sorted(glob(ROOT_DIR%(month,month,day)))
             if len(files)==0:
                 continue
             num_n = 0  
             num_d = 0
             for filename in files:
-                print  os.path.basename(filename)
+                print(os.path.basename(filename))
+                #caObj_new=readTruthImagerMatchObj(filename, truth=truth) 
                 try:
-                    caObj_new=readTruthImagerMatchObj(filename, truth_read=truth) 
+                    caObj_new=readTruthImagerMatchObj(filename, truth=truth) 
                 except:
-                    print "problem with", os.path.basename(filename)
+                    print("problem with", os.path.basename(filename))
                     continue
                 #if caObj_new.cloudsat.RVOD_CWC_status is None or len(caObj_new.cloudsat.RVOD_CWC_status) != len(caObj_new.avhrr.cpp_lwp):
                 #    print("Missing RVOD_CWC_status")
@@ -48,14 +49,14 @@ for year in [2010]
                 if int(os.path.basename(filename).split('_')[3][0:2])<12:
                     #time between 0000 and 11:59                
                     num_n +=1
-                    print "reading",os.path.basename(filename)
+                    print("reading",os.path.basename(filename))
                     if caObj_night is None:
                         caObj_night =  caObj_new  
                     else:    
                         caObj_night = caObj_night + caObj_new             
                 else :
                     num_d +=1
-                    print "reading",os.path.basename(filename)
+                    print("reading",os.path.basename(filename))
                     if caObj_day is None:
                         caObj_day =  caObj_new  
                     else:    
@@ -64,13 +65,13 @@ for year in [2010]
                 filename_night = outfile_template%(month,day,"00")
                 outfile = os.path.join(OUT_DIR, filename_night)
                 writeTruthImagerMatchObj(
-                    outfile, caObj_night, SETTINGS, avhrr_obj_name = 'pps')   
+                    outfile, caObj_night, SETTINGS, imager_obj_name = 'pps')   
                 caObj_night = None
 
             if num_d>0:
                 filename_day = outfile_template%(month,day,"12")
                 outfile = os.path.join(OUT_DIR, filename_day)
-                writeCaliopAvhrrMatchObj(
-                    outfile, caObj_day, SETTINGS, avhrr_obj_name = 'pps')   
+                writeTruthImagerMatchObj(
+                    outfile, caObj_day, SETTINGS, imager_obj_name = 'pps')   
                 caObj_day = None 
 
