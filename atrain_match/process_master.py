509306dddfc2b4eed0015b8ac18bb39a3afcacf1
Nina Hakansson <nina.hakansson@smhi.se>
2019-08-20 09:12:47 +0200

    Updated headers

--- atrain_match/process_master.py
+++ atrain_match/process_master.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2009-2019 atrain_match developers
+#
+# This file is part of atrain_match.
+#
+# atrain_match is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# atrain_match is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with atrain_match.  If not, see <http://www.gnu.org/licenses/>.
 """
 This module is the main entry point for matching noaa and/or metop data with
 Cloudsat and Calipso data, and produce statistics for validation of PPS
--- python_edit_the_code.py
+++ python_edit_the_code.py
@@ -1,8 +1,27 @@
 import re, glob, os
-ROOT_DIR = "/home/a001865/git/rename_atrainmatch/atrain_match/"
+ROOT_DIR = "/home/a001865/git/atrain_match/atrain_match/"
 files = glob.glob(ROOT_DIR + "/*.py")
 files = files + glob.glob(ROOT_DIR + "/*/*.py")
 files = files + glob.glob(ROOT_DIR + "/*/*/*.py")
+
+NEW_Header = (
+    "# -*- coding: utf-8 -*-\n"
+    "# Copyright (c) 2009-2019 atrain_match developers\n"
+    "#\n"
+    "# This file is part of atrain_match.\n"
+    "#\n"
+    "# atrain_match is free software: you can redistribute it and/or modify it\n"
+    "# under the terms of the GNU General Public License as published by\n"
+    "# the Free Software Foundation, either version 3 of the License, or\n"
+    "# (at your option) any later version.\n"
+    "#\n"
+    "# atrain_match is distributed in the hope that it will be useful, but\n"
+    "# WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+    "# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU\n"
+    "# General Public License for more details.\n"
+    "#\n"
+    "# You should have received a copy of the GNU General Public License\n"
+    "# along with atrain_match.  If not, see <http://www.gnu.org/licenses/>.\n")
 
 
 var_name_dict ={
@@ -20,22 +39,27 @@ var_name_dict ={
 for filename in files:
     if os.path.basename(filename) in "python_edit_the_code.py":
         continue
-        print "do not edit %s"%(os.path.basename(filename))
-    print filename 
+        print("do not edit %s"%(os.path.basename(filename)))
+    print(filename )
     all_file=""
     python_file = open(filename,'r') 
-
-    for line in python_file:
+    line_list = [line for line in python_file]
+    first_line = line_list[0]
+    if "#!" in first_line:
+        all_file = first_line 
+        line_list.pop(0) 
+    all_file = all_file +  NEW_Header   
+    for line in line_list:
         #line = line.replace("avhrr", "imager")
         #line = line.replace("AVHRR", "IMAGER")
         #line = line.replace("Avhrr", "Imager")
         #line = line.replace("nnImager", "nnAvhrr")
         #line = line.replace("nnavhrr", "nnimager")
         #line = line.replace("NN-IMAGER", "NN-AVHRR")
-        line = line.replace("cloudsat_calipso_imager", "truth_imager")
+        #line = line.replace("cloudsat_calipso_imager", "truth_imager")
         #if "_amsr" not in line:
         #    line = line.replace("amsr_imager", "match_util")
-        line = line.replace("match_match_util", "match_amsr_imager")
+        #line = line.replace("match_match_util", "match_amsr_imager")
         #line = re.sub(r"\.elevation", '.DEM_surface_elevation',line)
         #if re.search("alipso\.elevation",line) and 1==2:
         #    line = line.rstrip()
@@ -65,8 +89,8 @@ for filename in files:
         #              'profile_utc_time',line)
         #line = re.sub(r"time_tai", 
         #              'profile_time_tai',line)
-        line = re.sub(r"feature_optical_depth_532_top_layer5km", 
-                      'feature_optical_depth_532_top_layer_5km',line)
+        #line = re.sub(r"feature_optical_depth_532_top_layer5km", 
+        #              'feature_optical_depth_532_top_layer_5km',line)
 
         """Maybe not do this!!
         line = re.sub(r"alipso\.time", 
