936ff533facdc8cd038638b303f08ac99968a33f
Nina Hakansson <nina.hakansson@smhi.se>
2019-11-29 18:37:39 +0100

    Flake8

--- atrain_match/tests/test_plot_kuipers_on_map.py
+++ atrain_match/tests/test_plot_kuipers_on_map.py
@@ -15,54 +15,55 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with atrain_match.  If not, see <http://www.gnu.org/licenses/>.
-# test_match.py
-"""for the match module match.py """
-#
+
+"""Tests for the calculation measurements on fibonacci grid."""
+
+
 import numpy as np
 import unittest
 from atrain_match.reshaped_files_scr.plot_kuipers_on_area_util import StatsOnFibonacciLattice
 
 
 class test_kuipers_plot_on_map(unittest.TestCase):
+    """Test the calcualtions made on fibbonaci grid."""
 
     def setUp(self):
-        self.area = StatsOnFibonacciLattice()
-        self.area.num_detected_clouds = np.array([10, 1000, 1000, 1000, 1000, 1000])
-        self.area.num_undetected_clouds = np.array([1000, 100, 100, 100, 100, 100])
-        self.area.num_detected_clear = np.array([20, 80, 100, 0, 200, 100])
-        self.area.num_false_clouds = np.array([80, 20, 1, 1, 200, 300])
-        self.area.lats = np.array([80, 20, 1, 1, 200, 300])
+        """Set up a finbonacci gird with some data."""
+        self.lattice = StatsOnFibonacciLattice()
+        self.lattice.num_detected_clouds = np.array([10, 1000, 1000, 1000, 1000, 1000])
+        self.lattice.num_undetected_clouds = np.array([1000, 100, 100, 100, 100, 100])
+        self.lattice.num_detected_clear = np.array([20, 80, 100, 0, 200, 100])
+        self.lattice.num_false_clouds = np.array([80, 20, 1, 1, 200, 300])
+        self.lattice.lats = np.array([80, 20, 1, 1, 200, 300])
 
         dummy = 0 * np.array([80, 20, 1, 1, 200, 300])
-        self.area.sum_ctth_mae_low = dummy
-        self.area.sum_ctth_mae_high = dummy
-        self.area.sum_ctth_mae = dummy
-        self.area.sum_ctth_mae_diff = dummy
+        self.lattice.sum_ctth_mae_low = dummy
+        self.lattice.sum_ctth_mae_high = dummy
+        self.lattice.sum_ctth_mae = dummy
+        self.lattice.sum_ctth_mae_diff = dummy
 
-        self.area.sum_ctth_bias_low = dummy
-        self.area.sum_lapse_bias_low = dummy
-        self.area.sum_ctth_bias_temperature_low = dummy
-        self.area.sum_ctth_bias_temperature_low_t11 = dummy
-        self.area.Min_lapse_rate = dummy
-        self.area.num_new_detected_clouds = dummy
-        self.area.num_new_false_clouds = dummy
-        self.area.num_detected_height_low = dummy
+        self.lattice.sum_ctth_bias_low = dummy
+        self.lattice.sum_lapse_bias_low = dummy
+        self.lattice.sum_ctth_bias_temperature_low = dummy
+        self.lattice.sum_ctth_bias_temperature_low_t11 = dummy
+        self.lattice.Min_lapse_rate = dummy
+        self.lattice.num_new_detected_clouds = dummy
+        self.lattice.num_new_false_clouds = dummy
+        self.lattice.num_detected_height_low = dummy
 
     def test_calculate_kuipers(self):
-        self.area.calculate_kuipers()
-        # print
-        # print self.area.Kuipers
-        self.assertTrue(np.abs(self.area.Kuipers[0] + 0.79) < 0.01)
-        self.assertTrue(np.abs(self.area.Kuipers[1] - 0.709) < 0.01)
-        self.assertTrue(np.abs(self.area.Kuipers[2] - 0.899) < 0.01)
-        self.assertTrue(self.area.Kuipers.mask[3])
-        self.assertTrue(np.abs(self.area.Kuipers[4] - 0.40) < 0.01)
-        self.assertTrue(np.abs(self.area.Kuipers[5] - 0.15) < 0.01)
+        """Test kuipers calculation."""
+        self.lattice.calculate_kuipers()
+        self.assertTrue(np.abs(self.lattice.Kuipers[0] + 0.79) < 0.01)
+        self.assertTrue(np.abs(self.lattice.Kuipers[1] - 0.709) < 0.01)
+        self.assertTrue(np.abs(self.lattice.Kuipers[2] - 0.899) < 0.01)
+        self.assertTrue(self.lattice.Kuipers.mask[3])
+        self.assertTrue(np.abs(self.lattice.Kuipers[4] - 0.40) < 0.01)
+        self.assertTrue(np.abs(self.lattice.Kuipers[5] - 0.15) < 0.01)
 
 
 def suite():
-    """The suite for test_utils.
-    """
+    """Test suite for test remap measurements on fibonacci grid."""
     loader = unittest.TestLoader()
     mysuite = unittest.TestSuite()
     mysuite.addTest(loader.loadTestsFromTestCase(test_kuipers_plot_on_map))
