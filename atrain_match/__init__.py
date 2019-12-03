3ece339225e9f4aa8c56666ed00258a90698e294
Nina Hakansson <nina.hakansson@smhi.se>
2019-12-03 14:46:39 +0100

    Improved documentation

--- atrain_match/__init__.py
+++ atrain_match/__init__.py
@@ -20,7 +20,8 @@
 
 import numpy as np
 
-from atrain_match.version import __version__
+from atrain_match.version import __version__  # noqa F401
+
 
 def centered_modulus(array, divisor):
     """Transform array to half open range ]-divisor/2, divisor/2]."""
@@ -28,23 +29,30 @@ def centered_modulus(array, divisor):
     arr[arr > divisor / 2] -= divisor
     return arr
 
+
 def get_fibonacci_grid(radius_km, half_num_points=None):
-    # Earth area = 510072000km2
-    # 4000 point with radius~200km
-    # 1000 point with radium~100km
-    # 25000 radius 80km
-    # 64000 radius 5km
-    if half_num_points is None:
-        EARTH_AREA = 510072000
-        POINT_AREA = radius_km * radius_km * np.pi
-        half_num_points = int(EARTH_AREA / POINT_AREA)
+    """Return lon lat for fibonacci grid with requested radius, or half_num_points.
+
+    Method as in:
     # http://arxiv.org/pdf/0912.4540.pdf
     # Alvaro Gonzalez: Measurement of areas on sphere usig Fibonacci and latitude-longitude grid.
+
+    Relation number of points and radius of the points:
+    number of points    radius
+        4000             200km
+       16000             100km
+       65000              50km
+     1625000              10km
+
+    """
+    if half_num_points is None:
+        EARTH_AREA = 510072000  # Earth area (km2)
+        POINT_AREA = radius_km * radius_km * np.pi
+        half_num_points = int(0.5 * EARTH_AREA / POINT_AREA)
     num_points = 2 * half_num_points + 1
     latitude = np.rad2deg(np.arcsin(np.linspace(-1.0, 1.0, num_points)))
     lin_space = np.linspace(-half_num_points, half_num_points, num_points)
     theta = (1 + np.sqrt(5)) * 0.5
     longitude = 360 * (lin_space % theta) / theta
     longitude = centered_modulus(longitude, 360)
-
     return longitude, latitude
--- atrain_match/tests/test_init.py
+++ atrain_match/tests/test_init.py
@@ -29,13 +29,28 @@ from atrain_match.reshaped_files_scr.plot_kuipers_on_area_util import get_fibona
 class TestInit(unittest.TestCase):
     """Test function for the angle calculation."""
 
-    def test_get_fibonacci_grid(self):
-        """Test get_fibonacci_grid."""
+    def test_get_fibonacci_grid_compare_to_old_method(self):
+        """Test get_fibonacci_grid compared to old code."""
         lons, lats = get_fibonacci_grid(200, half_num_points=2010)
-        lonsold, latsold =  get_fibonacci_spread_points_on_earth(200, num_points=4021)
+        lonsold, latsold = get_fibonacci_spread_points_on_earth(200, num_points=4021)
         np.testing.assert_allclose(lats, latsold, atol=3.0)
         np.testing.assert_allclose(lons[:-1], lonsold[1:], atol=3.0)
-    
+
+    def test_get_fibonacci_grid(self):
+        """Test get_fibonacci_grid."""
+        expected_lons = np.array([-32.46117975, -169.9689438, 52.52329215, -84.9844719,
+                                  137.50776405, 0, -137.50776405, 84.9844719,
+                                  -52.52329215, 169.9689438, 32.46117975])
+        expected_lats = np.array([-90, -53.13010235, -36.86989765, -23.57817848,
+                                  -11.53695903, 0, 11.53695903, 23.57817848,
+                                  36.86989765, 53.13010235, 90.])
+        lons, lats = get_fibonacci_grid(200, half_num_points=5)
+        np.testing.assert_almost_equal(lats, expected_lats, decimal=3)
+        np.testing.assert_almost_equal(lons, expected_lons, decimal=3)
+        self.assertTrue(get_fibonacci_grid(200)[0].shape[0] - 4000 < 200)
+        self.assertTrue(get_fibonacci_grid(100)[0].shape[0] - 16000 < 500)
+        self.assertEqual(get_fibonacci_grid(200, half_num_points=1000)[0].shape[0], 2001)
+
     def test_centered_modulus(self):
         """Test centered_modulus."""
         angles = np.ma.array(
@@ -50,6 +65,7 @@ class TestInit(unittest.TestCase):
              [-3.0,  172.0, 164.0, 156.0]], mask=False)
         transformed = centered_modulus(angles, 360.0)
         np.testing.assert_allclose(transformed, expected)
+
 
 def suite():
     """Test non angle functions in pygac init file."""
