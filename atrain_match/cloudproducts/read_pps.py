1c30bc4fc00d7a06c60d83c61b58f6ec5e7e91ef
Nina.Hakansson <a001865@c21515.ad.smhi.se>
2020-09-07 11:44:00 +0200

    Bugfix from previous commit

--- atrain_match/cloudproducts/read_pps.py
+++ atrain_match/cloudproducts/read_pps.py
@@ -461,7 +461,7 @@ def read_imager_data_nc(pps_nc):
             image = pps_nc.variables[var]
             if not hasattr(image, 'id_tag'):
                 continue
-            if id_tag in ["qual_flags"]:
+            if image.id_tag in ["qual_flags"]:
                 continue
             if image.id_tag in ['satzenith', 'sunzenith',
                                 'azimuthdiff',
