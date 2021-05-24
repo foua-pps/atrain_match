e5cfe1ab155f2ed9c2a0a49970712890f7896eb7
Nina.Hakansson <a001865@c21515.ad.smhi.se>
2021-05-24 09:05:44 +0200

    bugfix enrgy_532

--- atrain_match/libs/truth_imager_statistics_lib.py
+++ atrain_match/libs/truth_imager_statistics_lib.py
@@ -260,7 +260,7 @@ def get_subset_for_mode(match_obj, mode):
 
 
     if match_obj.truth_sat.lower() in ['calipso']:
-        if hasattr(match_obj_truth_sat, 'minimum_laser_energy_532'):
+        if getattr(match_obj_truth_sat, 'minimum_laser_energy_532', None) is not None:
             energy = getattr(match_obj_truth_sat, 'minimum_laser_energy_532')
             cal_subset = np.logical_and(cal_subset, energy > 0.08)
     return cal_subset
